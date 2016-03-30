7df49fb4baf8bffd6e64d047c7b813aa10e919a1
Ümit TOPAÇAN <UMITTOPACAN@kkb.com.tr>
2016-03-30 20:39:15 +0300

    Initial commit

---
+++ app/src/main/java/com/ut/mis49m/fragmentlab/ListItem.java
@@ -0,0 +1,10 @@
+package com.ut.mis49m.fragmentlab;
+
+public class ListItem {
+
+	static String[] items = { "Item One", "Item Two" };
+
+	static String[] texts = {
+			"Text of item 1",
+			"Text of item 2" };
+}
